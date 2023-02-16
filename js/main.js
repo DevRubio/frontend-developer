c448becceb941c88ce8234b50e092674db8d04f5
oscar.franco <ofrubio0@gmail.com>
2023-02-16 15:46:38 -0500

    Se agrega el detalle del producto al index.html

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const desktopMenu= document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const MenuShoppingCard = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCardContainer = document.querySelector('#shoppingCardContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
@@ -11,18 +11,18 @@ menuHamIcon.addEventListener('click', toggleMobileMenu)
 MenuShoppingCard.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
-    const isAsideClose = aside.classList.contains('inactive')
+    const isAsideClose = shoppingCardContainer.classList.contains('inactive')
     if(!isAsideClose){
-        aside.classList.add('inactive')
+        shoppingCardContainer.classList.add('inactive')
     }
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMobileMenu(){
-    const isAsideClose = aside.classList.contains('inactive')
+    const isAsideClose = shoppingCardContainer.classList.contains('inactive')
 
     if(!isAsideClose){
-        aside.classList.add('inactive')
+        shoppingCardContainer.classList.add('inactive')
     }
     mobileMenu.classList.toggle('inactive')
 }
@@ -34,7 +34,7 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive')
         
     }
-    aside.classList.toggle('inactive')
+    shoppingCardContainer.classList.toggle('inactive')
     desktopMenu.classList.add('inactive')
 
 }
